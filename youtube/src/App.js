46eee1d0d72d9b9cb441c56d3bba3bd4004676b9
kartikgupta666 <kartikgangil@gmail.com>
2024-02-16 00:11:26 +0530

    combine sindenavbar and main navbar in one component

--- youtube/src/App.js
+++ youtube/src/App.js
@@ -2,13 +2,13 @@
 import './App.css';
 import Content from './component/Content';
 import Navbar from './component/Navbar';
-import Sidenavbar from './component/Sidenavbar';
+
 
 function App() {
   return (
    <>
    <Navbar/>
-   <Sidenavbar/>
+   
    <Content/>
    </>
   );
--- youtube/src/component/Navbar.js
+++ youtube/src/component/Navbar.js
@@ -1,14 +1,20 @@
 import React from 'react'
-// import './App.css';
+
 
 export default function Navbar() {
+
+    function show() {
+        document.getElementById('na').classList.toggle('active');
+        document.getElementById('na-2').classList.toggle('deactive')
+
+    }
 
     return (
         <div>
             <div className="nav">
                 <nav>
                     <div className="logo">
-                        <button id="slider" className="btn"
+                        <button id="slider" onClick={show} className="btn"
                             style={{ "position": "relative", "left": "-90px", "fontSize": "30px" }}>
                             <i className="fas fa-sliders"></i></button>
 
@@ -31,6 +37,32 @@ export default function Navbar() {
 
                     </div>
                 </nav>
+            </div>
+            {/* <!-- side-nav bar --> */}
+            <div className="side-nav-bar" id="snv">
+                {/* <!-- 1st side-nav bar --> */}
+
+                {/* <!-- 2nd side-nav bar --> */}
+                <div className="na-2" id="na-2">
+                    <ul>
+                        {/* <!-- home --> */}
+                        <li className="bu"><i className='fas fa-home' style={{ " fontSize": " 25px" }}></i><span>Home</span></li>
+                        {/* <!-- shorts --> */}
+                        <li className="bu"><i className='fas fa-play-circle' style={{ " fontSize": " 25px" }}></i><span>Shorts</span></li>
+                        {/* <!-- Subscription --> */}
+                        <li className="bu">
+                            <i className='far fa-folder' style={{ " fontSize": " 25px" }}></i>
+                            <i className="fas fa-play" style={{ "position": "relative", "left": "2px", " top": "-16px", " fontSize": "10px" }}></i>
+                            <span> Subscription</span>
+                        </li>
+                        {/* <!-- you --> */}
+                        <li className="bu">
+                            <i className='fas fa-user' style={{ " fontSize": " 25px" }}></i><span>You</span>
+                        </li>
+                        {/* <!-- download --> */}
+                        <li className="bu"><i className='fas fa-download' style={{ "bottom ": " 10px" }}></i><span>Download</span></li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
