b1fe9b9a7e8c24606a10b85f0b1546b1c15ffab8
kartikgupta666 <kartikgangil@gmail.com>
2024-02-16 00:03:23 +0530

    unused files deleated

--- youtube.js
+++
@@ -1,9 +0,0 @@
-let sidenavBar = document.querySelector('.na');
-let sidenavBar2 = document.querySelector('.na-2');
-
-function show(){
-    document.getElementById('na').classList.toggle('active');
-    document.getElementById('na-2').classList.toggle('deactive')
-
-  }
-// for using toggle property we must use style element...
